22745e65df62377efbe8dd69c928058beaad2595
蛇道 <100847059+caminodelaserpiente@users.noreply.github.com>
2026-08-25 12:05:48 -0600

    UPDATE: Debian security repository URLs

--- 02-CaminodelaserpienteOS.sh
+++ 02-CaminodelaserpienteOS.sh
@@ -35,8 +35,8 @@ main() {
 deb http://deb.debian.org/debian/ trixie main contrib non-free non-free-firmware
 deb-src http://deb.debian.org/debian/ trixie main contrib non-free non-free-firmware
 
-deb https://security.debian.org/debian-security trixie-security main contrib non-free non-free-firmware
-deb-src https://security.debian.org/debian-security trixie-security main contrib non-free non-free-firmware
+deb http://security.debian.org/debian-security trixie-security main contrib non-free non-free-firmware
+deb-src http://security.debian.org/debian-security trixie-security main contrib non-free non-free-firmware
 
 deb http://deb.debian.org/debian/ trixie-updates main contrib non-free non-free-firmware
 deb-src http://deb.debian.org/debian/ trixie-updates main contrib non-free non-free-firmware
